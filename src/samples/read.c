996f0348e158fcd985b3b74a0ed8e7d77e4637e7
Charles Tran <kdt57@drexel.edu>
2024-05-16 12:04:26 -0400

    added EPC, rssi, label for freeform object

--- src/samples/read.c
+++ src/samples/read.c
@@ -11,8 +11,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include <easy.h>
 #include <curl/curl.h>
+
+// Function to identify tags
+void printLabel(const char *epc, char *label) {
+    if (strcmp(epc, "E280116060000211EBDE7479") == 0) {
+        // printf("GRN\n");
+        sprintf(label, "%s", "GRN");
+    } else if (strcmp(epc, "E280116060000211EBDE7469") == 0) {
+        // printf("BLK\n");
+        sprintf(label, "%s", "BLK");
+    } else if (strcmp(epc, "E280116060000211EBDE7459") == 0) {
+        // printf("RED\n");
+        sprintf(label, "%s", "RED");
+    } else if (strcmp(epc, "E280116060000211EBDE7489") == 0) {
+        // printf("WHT\n");
+        sprintf(label, "%s", "WHT");
+    } else if (strcmp(epc, "E280116060000211EBDCDD2D") == 0) {
+        // printf("SMR\n");
+        sprintf(label, "%s", "SMR");
+    } else if (strcmp(epc, "E280116060000211EBDCDD3D") == 0) {
+        // printf("MAG\n");
+        sprintf(label, "%s", "MAG");
+    } else if (strcmp(epc, "E280116060000211EBDCDD4D") == 0) {
+        // printf("KDT\n");
+        sprintf(label, "%s", "KDT");
+    } else if (strcmp(epc, "E280116060000211EBDCDD5D") == 0) {
+        // printf("VPS\n");
+        sprintf(label, "%s", "VPS");
+    } else if (strcmp(epc, "E280116060000211EBDCDD6D") == 0) {
+        // printf("BAD\n");
+        sprintf(label, "%s", "BAD");
+    } else if (strcmp(epc, "E280116060000211EBDCDD7D") == 0) {
+        // printf("MLW\n");
+        sprintf(label, "%s", "MLW");
+    } else if (strcmp(epc, "E280116060000211EBDCDD8D") == 0) {
+        // printf("JDZ\n");
+        sprintf(label, "%s", "JDZ");
+    } else if (strcmp(epc, "E280116060000211EBDCDD9D") == 0) {
+        // printf("SPB\n");
+        sprintf(label, "%s", "SPB");
+    } else if (strcmp(epc, "E280116060000211EBDCDDAD") == 0) {
+        // printf("PAT\n");
+        sprintf(label, "%s", "PAT");
+    } else if (strcmp(epc, "E280116060000211EBDCDDBD") == 0) {
+        // printf("CEL\n");
+        sprintf(label, "%s", "CEL");
+    } else if (strcmp(epc, "E280116060000211EBDE7469") == 0) {
+        // printf("Vishnu IPhone\n");
+        sprintf(label, "%s", "VIP");
+    } else if (strcmp(epc, "E280116060000211EBDDEB7F") == 0) {
+        // printf("Vishnu Wallet\n");
+        sprintf(label, "%s", "WAL");
+    } else if (strcmp(epc, "E280116060000211EBDCDDCD") == 0) {
+        // printf("BOB\n");
+        sprintf(label, "%s", "BOB");
+    } else if (strcmp(epc, "E280116060000211EBDCDDDD") == 0) {
+        // printf("BIB\n");
+        sprintf(label, "%s", "BIB");
+    } else if (strcmp(epc, "E280116060000211EBDCDDED") == 0) {
+        // printf("LAB\n");
+        sprintf(label, "%s", "LAB");
+    } else if (strcmp(epc, "E280116060000211EBDD090D") == 0) {
+        // printf("FIB\n");
+        sprintf(label, "%s", "FIB");
+    } else if (strcmp(epc, "E280116060000211EBDD091D") == 0) {
+        // printf("NIB\n");
+        sprintf(label, "%s", "NIP");
+    } else if (strcmp(epc, "E280116060000211EBDD092D") == 0) {
+        // printf("TIB\n");
+        sprintf(label, "%s", "TIB");
+    } else if (strcmp(epc, "E280116060000211EBDD093D") == 0) {
+        // printf("BIN\n");
+        sprintf(label, "%s", "BIN");
+    } else if (strcmp(epc, "E280116060000211EBDD094D") == 0) {
+        // printf("SIB\n");
+        sprintf(label, "%s", "SIB");
+    } else if (strcmp(epc, "E280116060000211EBDD095D") == 0) {
+        // printf("LIB\n");
+        sprintf(label, "%s", "LIB");
+    } else if (strcmp(epc, "E280116060000211EBDD096D") == 0) {
+        // printf("DIB\n");
+        sprintf(label, "%s", "DIB");
+    } else {
+        // printf("Unknown\n");
+        sprintf(label, "%s", "Unknown");
+    }
+}
 
 // Function to get the current epoch time in microseconds
 long long get_epoch_time_microseconds() {
@@ -29,6 +114,8 @@ long long get_epoch_time_microseconds() {
 
     return microseconds;
 }
+
+// Function to send HTTP request
 void sendPutRequest(const long long relative_time, const long long interrogator_time, const char *db_password, const char *freeform) {
     CURL *curl;
     CURLcode res;
@@ -44,10 +131,11 @@ void sendPutRequest(const long long relative_time, const long long interrogator_
             return;
         }
        
-        sprintf(jsonData, "[{\"data\":{\"relative_time\":\"%lld\",\"interrogator_time\":\"%lld\",\"db_password\":\"%s\",\"freeform\":\"%s\"}}]",
+        sprintf(jsonData, "[{\"data\":{\"relative_time\":\"%lld\",\"interrogator_time\":\"%lld\",\"db_password\":\"%s\",\"freeform\": %s}}]",
                 relative_time, interrogator_time, db_password, freeform);
- 
+        
         printf("jsonData: %s\n", jsonData);
+
         // Set the URL for the PUT request
         curl_easy_setopt(curl, CURLOPT_URL, "https://localhost:5001/api/rssi");
  
@@ -58,8 +146,8 @@ void sendPutRequest(const long long relative_time, const long long interrogator_
         // Set the content type header
         headers = curl_slist_append(headers, "Content-Type: application/json; charset=UTF-8");
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
- 	//curl_easy_setopt(curl, CURLOPT_PUT,1);
-	curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
+	      curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
+        
         // Perform the PUT request
         res = curl_easy_perform(curl);
         if(res != CURLE_OK) {
@@ -115,8 +203,6 @@ int main(int argc, char *argv[])
   TMR_Status ret;
   TMR_Region region;
   TMR_TransportListenerBlock tb;
-  // TMR_ReadPlan plan;
-  // uint8_t antennaList[] = {0x02, 0x01, 0x01};
 
   if (argc < 2)
   {
@@ -135,19 +221,11 @@ int main(int argc, char *argv[])
   TMR_addTransportListener(rp, &tb);
 #endif
 
-  // uint32_t baudRate = 9600;
-  // ret = TMR_paramSet(rp, TMR_PARAM_BAUDRATE, &baudRate);
-  // if (TMR_SUCCESS != ret)
-  // {
-  //   errx(1, "Failed to set baud rate: %s\n", TMR_strerr(rp, ret));
-  // }
-
   ret = TMR_connect(rp);
   checkerr(rp, ret, 1, "connecting reader");
   printf("connected successfully\n");
 
   region = 0x0D;
-  //ret = TMR_paramSet(rp, TMR_paramID("/reader/region/id"), &region);
   ret = TMR_paramSet(rp, TMR_PARAM_REGION_ID, &region);
   checkerr(rp, ret, 1, "setting region");
   printf("set region succesfully\n");
@@ -206,7 +284,13 @@ int main(int argc, char *argv[])
   long long start_time = get_epoch_time_microseconds();
   while (true)
   {
-      char buffer[100] = {0};
+      char label[100] = {0};
+      char *buffer = (char *)malloc(1024 * sizeof(char)); // Adjust size as needed;
+      if (!buffer) {
+            printf("Failed to allocate memory for buffer data.\n");
+            return;
+      }
+
       // Read tags
       printf("trying to read data\n");
       status = TMR_read(rp, 1000, NULL); // 500ms read time
@@ -227,11 +311,15 @@ int main(int argc, char *argv[])
 
       TMR_bytesToHex(trd.tag.epc, trd.tag.epcByteCount, epcStr);
       printf("EPC: %s, RSSI: %d dBm\n", epcStr, trd.rssi);
+    //   printf("Print Label: ");
+      printLabel(epcStr, label);
       printf("Sending data to the database...\n");
-      sprintf(buffer, "%d", trd.rssi);
-      printf("buffer: %s\n", buffer);
+      
+      sprintf(buffer, "[{\"EPC\":\"%s\",\"rssi\":\"%d\", \"Label\":\"%s\"}]", epcStr, trd.rssi, label);
+      
+    //   printf("buffer: %s\n", buffer);
       sendPutRequest(get_epoch_time_microseconds() - start_time, get_epoch_time_microseconds(), "kapilrocks", buffer);
-      // sendPutRequest("2024-04-19T12:00:00Z", "2024-04-19T12:00:05Z", "kapilrocks", "this is a test");
+      free(buffer);
   }
 
   TMR_destroy(rp);
